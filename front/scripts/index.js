19529a028b95210417014a34666026b4adc5c2ae
Alejo Holmann <alejoholmann99@gmail.com>
2024-11-14 22:06:21 -0300

    webpack

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,30 +1,4 @@
-//console.log(tempData);
-
+const execution = require("./rendercarts");
 $.get("https://students-api.up.railway.app/movies", (Data) => {
   execution(Data);
 });
-
-const execution = (Data) => {
-  const container = document.getElementById("cards");
-
-  Data.forEach((data) => {
-    const card = document.createElement("div");
-    card.className = "card";
-
-    card.innerHTML = `
-      <img src="${data.poster}" alt="${data.title}">
-      <div class="infocontainer">
-      <h2 class="titulo">${data.title}</h2>
-            <h3 class="subtitulo">${data.director} | ${data.year}</h3>
-            <p class="cuerpo">
-            Duración: ${data.duration}.</br>
-             Género: ${data.genre.map((el) => ` ${el}`)}.</br>
-             Puntuación: ${data.rate}.
-             </p>
-            <div class="pie"><a href="${data.link}">Más información</a></div>
-            </div>
-        `;
-
-    container.appendChild(card);
-  });
-};
---
+++ front/scripts/rendercarts.js
@@ -0,0 +1,26 @@
+const execution = (Data) => {
+  const container = document.getElementById("cards");
+
+  Data.forEach((data) => {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    card.innerHTML = `
+      <img src="${data.poster}" alt="${data.title}">
+      <div class="infocontainer">
+      <h2 class="titulo">${data.title}</h2>
+            <h3 class="subtitulo">${data.director} | ${data.year}</h3>
+            <p class="cuerpo">
+            Duración: ${data.duration}.</br>
+             Género: ${data.genre.map((el) => ` ${el}`)}.</br>
+             Puntuación: ${data.rate}.
+             </p>
+            <div class="pie"><a href="${data.link}">Más información</a></div>
+            </div>
+        `;
+
+    container.appendChild(card);
+  });
+};
+
+module.exports = execution;
